c18dae5458e98943197fdebc7644317cd75d8aeb
andrewbelles <andrewbelles31@gmail.com>
2025-08-16 18:03:09 -0400

    finished multiplier, reduced wattage by 17W by pipelining. increasing time to 3 cycles

--- VHDL/multiplier.vhd
+++ VHDL/multiplier.vhd
@@ -6,6 +6,7 @@ entity multiplier_16b16 is
 port (
   clk_port     : in std_logic;
   load_port    : in std_logic;  
+  reset_port   : in std_logic; 
   A, B         : in std_logic_vector(15 downto 0);
   A_dec_count  : in std_logic_vector(3 downto 0);
   B_dec_count  : in std_logic_vector(3 downto 0); 
@@ -26,10 +27,9 @@ architecture behavioral of multiplier_16b16 is
 
   -- base type signals 
   signal shift_count   : unsigned(4 downto 0) := (others => '0'); 
-  signal S, A_mag32    : unsigned(31 downto 0) := (others => '0');    
+  signal A_mag32    : unsigned(31 downto 0) := (others => '0');    
   signal B_mag16       : unsigned(15 downto 0) := (others => '0');
-  signal negative_flag : std_logic := '0';
-
+  signal negative_flag, reset : std_logic := '0';
   -- for fsm 
   signal current_state, next_state : state_type := idle; 
 begin 
@@ -43,40 +43,45 @@ begin
   end if; 
 end process update_state; 
 
-next_state_logic: process( current_state )
+next_state_logic: process( current_state, load_port, reset_port ) 
 begin
-  case (current_state) is 
-    when idle =>
-      next_state <= idle; 
-      if load_port = '1' then 
-        current_state <= first; 
-      end if; 
-    when first => 
-      next_state <= second; 
-    when second =>  
-      next_state <= third; 
-    when third => 
-      next_state <= done; 
-    when done => 
-      next_state <= idle; 
-  end case; 
+  if reset_port = '1' then
+    next_state <= idle; 
+  else 
+    case (current_state) is 
+      when idle =>
+        next_state <= idle; 
+        if load_port = '1' then 
+          next_state <= first; 
+        end if; 
+      when first =>
+        next_state <= second; 
+      when second =>  
+        next_state <= third; 
+      when third => 
+        next_state <= done; 
+      when done => 
+        next_state <= idle; 
+    end case; 
+  end if;
 end process next_state_logic; 
 
-output_logic: process ( current_state )
+output_logic: process ( current_state, partials, negative_flag, shift_count )
+  variable S : unsigned(31 downto 0) := (others => '0');
 begin
-  
+  reset <= '0';
   case ( current_state) is 
     when idle => 
-      -- TODO: reset  
+      reset <= '1'; 
     when done => 
-      stage_counter <= (others => '0');
+      S := partials(0);
       if negative_flag = '0' then 
-        AB <= std_logic_vector( signed(S(15 downto 0)) )
+        AB <= std_logic_vector( signed(S(15 downto 0)) );
       else 
-        AB <= std_logic_vector(-signed(S(15 downto 0)) ) when others;
+        AB <= std_logic_vector(-signed(S(15 downto 0)) );
       end if; 
       -- 5 bits - value 8 guarentees value is 4 bits or less 
-      AB_dec_count <= std_logic_vector((shift_count));
+      AB_dec_count <= std_logic_vector((shift_count(3 downto 0)));
     when others => 
       null;
   end case; 
@@ -88,14 +93,21 @@ end process output_logic;
 load_magnitudes: process( clk_port )
 begin 
   if rising_edge( clk_port ) then 
-    -- enable
-    if load_port = '1' then 
+    if reset = '1' then 
+
+      A_mag32 <= (others => '0'); 
+      B_mag16 <= (others => '0');
+      partials <= (others => (others => '0'));
+      mag32_shifts <= (others => (others => '0'));
+      AB <= (others => '0');
+
+    elsif load_port = '1' then 
       -- get 2's complement of input values if negative 
       -- store in flip flop
       if (A(15) = '1') then 
-        A_mag32 <= (others => '0') & unsigned(not(A)) + 1; 
+        A_mag32 <= x"0000" & unsigned(not(A)) + 1; 
       else 
-        A_mag32 <= (others => '0') & unsigned(A);
+        A_mag32 <= x"0000" & unsigned(A);
       end if; 
 
       if (B(15) = '1') then 
@@ -111,8 +123,16 @@ end process load_magnitudes;
 load_auxilliary: process( clk_port )
 begin 
   if rising_edge( clk_port) then 
-    if load_port = '1' then 
-      negative_flag <= '1' when A(15) XOR B(15) else '0';  
+    if reset = '1' then 
+      negative_flag <= '0'; 
+      shift_count <= (others => '0');
+
+    elsif load_port = '1' then 
+      if (A(15) xor B(15)) = '1' then 
+        negative_flag <= '1';
+      else 
+        negative_flag <= '0';
+      end if;  
 
       -- if number of decimals is less than 8 then we shouldn't shift
       if (('0' & unsigned(A_dec_count)) + ('0' & unsigned(B_dec_count))) < 8 then
@@ -128,28 +148,34 @@ end process load_auxilliary;
 -- pipelines the sum across 3 cycles 
 -- sum is stored in 0th of partials array
 pipeline_sum: process( clk_port )
-  variable partial_sum : unsigned(31 downto 0);   
+  variable partial_sum : unsigned(31 downto 0) := (others => '0');   
   variable to_shift : integer := 0; 
 begin 
-  partial_sum := (others => '0');
-  case current_state is 
-    -- get four partial sums 
-    when first => 
-      for i in 0 to 3 loop 
-        to_shift := to_integer( shift_left(to_unsigned(i), 2) );
-        for k in 0 to 3 loop 
-          partial_sum := partial_sum + mag32_shifts(i + to_shift)
+  if rising_edge( clk_port ) then
+    case current_state is 
+      -- get four partial sums 
+      when first => 
+        for i in 0 to 3 loop 
+          partial_sum := (others => '0');
+          to_shift := to_integer( shift_left(to_unsigned(i, 4), 2) );
+          for j in 0 to 3 loop 
+            if B_mag16(j + to_shift) = '1' then
+              partial_sum := partial_sum + mag32_shifts(j + to_shift);
+            end if; 
+          end loop; 
+          partials(i) <= partial_sum;
         end loop; 
-        partials(i) <= partial_sum;
-      end loop; 
-    -- accumulate down to 2
-    when second => 
-        partials(0) <= partials(0) + partials(1);
-        partials(2) <= partials(2) + partials(3);
-    -- final sum 
-    when third => 
-      partials(0) <= partials(0) + partials(2);
-  end case; 
+      -- accumulate down to 2
+      when second => 
+          partials(0) <= partials(0) + partials(1);
+          partials(2) <= partials(2) + partials(3);
+      -- final sum 
+      when third => 
+        partials(0) <= partials(0) + partials(2);
+      when others => 
+        null;
+      end case; 
+  end if;
 end process pipeline_sum;
 
 ----------
