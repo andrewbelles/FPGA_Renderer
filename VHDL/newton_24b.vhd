b8e3ea88ba414f6f92b9b2e0660715c06a17097e
andrewbelles <andrewbelles31@gmail.com>
2025-08-24 16:03:08 -0400

    removed multiplier entity from logic in projection math

--- VHDL/newton_24b.vhd
+++ VHDL/newton_24b.vhd
@@ -15,195 +15,43 @@ entity newton_24b is
 end entity newton_24b; 
 
 architecture behavioral of newton_24b is 
------------------------ components ---------------------------------------
-component multiplier_24x24 
-  port (
-    clk_port   : in std_logic;
-    load_port  : in std_logic;  
-    reset_port : in std_logic; 
-    A, B       : in std_logic_vector(23 downto 0);
-    A_dig      : in std_logic_vector(4 downto 0);
-    B_dig      : in std_logic_vector(4 downto 0); 
-    AB         : out std_logic_vector(23 downto 0);
-    AB_dig     : out std_logic_vector(4 downto 0);
-    set_port   : out std_logic); 
-end component multiplier_24x24; 
 ----------------------- declarations -------------------------------------
 -- state declarations 
-  type state_type is ( idle, bufr1, prod1, bufr2, prod2, done ); 
-  signal current_state, next_state : state_type := idle;
-
--- enables 
-  signal reset_en      : std_logic := '0';
-  signal set_en        : std_logic := '0';
-  signal mul_en        : std_logic := '0';
-  signal first_mul_en  : std_logic := '0';
-  signal second_mul_en : std_logic := '0';
-
--- auxilliary signals 
-  signal mul_set       : std_logic := '0'; 
-  signal clear_mul     : std_logic := '0';
-  signal iteration     : integer := 0; 
-
--- signals for multiplication 
-  signal A, B          : std_logic_vector(23 downto 0) := (others => '0'); 
-  signal A_dig, B_dig  : std_logic_vector(4 downto 0) := (others => '0'); 
-  signal AB_dig        : std_logic_vector(4 downto 0) := (others => '0'); 
-  signal AB            : std_logic_vector(23 downto 0) := (others => '0');
-  signal diff, sRoot   : signed(23 downto 0) := (others => '0');
-  signal prod_helper   : signed(23 downto 0) := (others => '0');
+  signal iter   : unsigned(1 downto 0) := "00";
+  signal x0     : signed(23 downto 0) := (others => '0');
+  signal x1, x2 : signed(23 downto 0) := (others => '0');
+  signal m      : signed(23 downto 0) := (others => '0');
 
 -- 2 represented as a 11.12 fixed point value 
   constant two_1112    : signed(23 downto 0) := x"002000";
 begin 
 
---------------------------------------------------------------------------
--- Call to multiplier  
---------------------------------------------------------------------------
-multiply: multiplier_24x24
- port map(
-    clk_port   => clk_port,
-    load_port  => mul_en,
-    reset_port => clear_mul, 
-    A          => A,
-    B          => B,
-    A_dig      => A_dig,
-    B_dig      => B_dig,
-    AB         => AB, -- 11.12
-    AB_dig     => AB_dig,
-    set_port   => mul_set);
+-- load constant values
+x0 <= signed(seed);
+m  <= signed(mantissa);
 
--- set as flopped output values 
-root     <= std_logic_vector(sRoot);
-set_port <= set_en; 
-
---------------------------------------------------------------------------
--- Select proper operands for multiplication (Async) 
---------------------------------------------------------------------------
-set_B: process( iteration, seed, sRoot )
+newtons_method: process( clk_port ) 
 begin 
-  if iteration = 0 then 
-    B <= seed;  
-  else 
-    B <= std_logic_vector(shift_left(sRoot, 5));
-  end if; 
-end process set_B;
-
-A     <= mantissa               when first_mul_en = '1' else 
-         std_logic_vector(diff) when second_mul_en = '1' else
-         (others => '0');
-
-A_dig <= "10110" when first_mul_en = '1' else 
-         "01100" when second_mul_en = '1' else 
-         "00000";
-B_dig <= "10001";
-
---------------------------------------------------------------------------
--- Set intermediate and final values in memory 
---------------------------------------------------------------------------
-
-prod_helper <= signed(AB);
-set_prods: process( clk_port )
-begin 
-
-  if rising_edge( clk_port ) then 
-    if reset_en = '1' then 
-      iteration <= 0; 
-      diff <= (others => '0');
-      sRoot <= (others => '0');
-
-    elsif mul_set = '1' then 
-      -- safe to clear now 
-      if first_mul_en = '1' then 
-        diff <= two_1112 - prod_helper;
-
-      elsif second_mul_en = '1' then 
-        -- increment iteration 
-        iteration <= iteration + 1; 
-        sRoot <= prod_helper;
+  if rising_edge( clk_port ) then  
+    if reset_port = '1' then 
+      x1 <= (others => '0'); 
+      x2 <= (others => '0');
+    elsif iter = 2 then 
+      iter <= "00"; -- go back to low one cycle after  
+    elsif load_port = '1' then 
+      if iter = 0 then 
+        x1 <= x0 * (two_1112 - (m * x0)); 
+        iter <= iter + 1; 
+      else 
+        x2 <= x1 * (two_1112 - (m * x1));
+        iter <= iter + 1; 
       end if; 
     end if; 
   end if; 
-end process set_prods; 
+end process newtons_method;
 
---------------------------------------------------------------------------
--- FSM Logic 
---------------------------------------------------------------------------
-next_state_logic: process( current_state, reset_port, load_port, mul_set, iteration )
-begin 
-  if reset_port = '1' then 
-    next_state <= idle; 
-  else 
-    case ( current_state ) is
-      when idle => 
-        next_state <= idle; 
-        if load_port = '1' then 
-          next_state <= bufr1; 
-        end if; 
-      when bufr1 => 
-        next_state <= bufr1; 
-        -- mul set must go low from clear before proceeding 
-        if mul_set = '0' then 
-          next_state <= prod1;
-        end if; 
-      when prod1 => 
-        next_state <= prod1; 
-        if mul_set = '1' then 
-          next_state <= bufr2; 
-        end if; 
-      when bufr2 => 
-        next_state <= bufr2; 
-        -- mul set must go low from clear before proceeding 
-        if mul_set = '0' then 
-          next_state <= prod2;
-        end if; 
-      when prod2 => 
-        next_state <= prod2; 
-        if mul_set = '1' then 
-          if iteration = 1 then 
-            next_state <= done; 
-          else 
-            next_state <= bufr1; 
-          end if; 
-        end if; 
-      when done => 
-        next_state <= done; 
-    end case; 
-  end if; 
-end process next_state_logic; 
-
-output_logic: process( current_state )
-begin 
-  reset_en      <= '0';
-  first_mul_en  <= '0';
-  second_mul_en <= '0';
-  mul_en        <= '0';
-  set_en        <= '0';
-  clear_mul     <= '0';
-
-  case ( current_state ) is 
-    when idle => 
-      reset_en <= '1';
-    when bufr1 => 
-      clear_mul <= '1';
-    when prod1 => 
-      first_mul_en <= '1';
-      mul_en   <= '1';
-    when bufr2 => 
-      clear_mul <= '1';
-    when prod2 =>
-      second_mul_en <= '1';
-      mul_en   <= '1';
-    when done => 
-      set_en   <= '1';
-  end case; 
-end process output_logic; 
-
-update_state: process( clk_port )
-begin 
-  if rising_edge( clk_port ) then 
-    current_state <= next_state; 
-  end if; 
-end process update_state; 
+--- enable set once iter goes high 
+set_port <= '1' when iter = 2 else '0'; 
+root <= std_logic_vector(iter);
 
 end architecture behavioral;
--- VHDL/projection.vhd
+++ VHDL/projection.vhd
@@ -25,41 +25,15 @@ component reciprocal_24b
     set_port   : out std_logic); 
 end component reciprocal_24b; 
 
-component multiplier_24x24 
-  port (
-    clk_port   : in std_logic;
-    load_port  : in std_logic;  
-    reset_port : in std_logic; 
-    A, B       : in std_logic_vector(23 downto 0);
-    A_dig      : in std_logic_vector(4 downto 0);
-    B_dig      : in std_logic_vector(4 downto 0); 
-    AB         : out std_logic_vector(23 downto 0);
-    AB_dig     : out std_logic_vector(4 downto 0);
-    set_port   : out std_logic); 
-end component multiplier_24x24; 
-  type state_type is ( idle, pool_vals, divide, intercept, done );
-  signal current_state, next_state : state_type := idle; 
+  signal reciprocal_set : std_logic := '0'; 
+  signal divide_set     : std_logic := '0';
+  signal reciprocal_sg  : std_logic_vector(23 downto 0) := (others => '0'); 
+  signal Wc_reciprocal  : signed(23 downto 0) := (others => '0');
+  signal xndc, yndc     : signed(23 downto 0) := (others => '0'); 
+  signal Xc, Yc         : signed(23 downto 0) := (others => '0');
 
-  signal inv_z          : signed(23 downto 0)           := (others => '0'); 
-  signal perspective    : std_logic_vector(23 downto 0) := (others => '0');
-  signal points         : array_2x24_t := (others => (others => '0'));
-  signal divided_points : array_2x24_t := (others => (others => '0'));
-  signal transl_points  : std_logic_vector(15 downto 0) := (others => '0');
-  signal pool_set       : std_logic_vector(2 downto 0)  := (others => '0');
-  signal divide_set     : std_logic_vector(1 downto 0)  := (others => '0');
-  signal pool_done      : std_logic := '0';
-  signal divide_done    : std_logic := '0';
-  signal intercept_done : std_logic := '0'; 
-
-  signal pool_en        : std_logic := '0';
-  signal reset_en       : std_logic := '0';
-  signal divide_en      : std_logic := '0'; 
-  signal intercept_en   : std_logic := '0';
-  signal set_en         : std_logic := '0';
-
-  constant m00          : std_logic_vector(23 downto 0) := x"0014c9";
-  constant m11          : std_logic_vector(23 downto 0) := x"001BB6";
-  constant d12          : std_logic_vector(4 downto 0)  := "01100";
+  constant m00          : signed(23 downto 0) := x"0014c9";
+  constant m11          : signed(23 downto 0) := x"001BB6";
   constant b            : signed(23 downto 0) := x"000080";
 begin 
 --------------------------------------------------------------------------
@@ -68,191 +42,56 @@ begin
 get_reciprocal: reciprocal_24b 
   port map( 
     clk_port   => clk_port, 
-    load_port  => pool_en,
-    reset_port => reset_en, 
+    load_port  => load_port,
+    reset_port => reset_port, 
     value      => z,
-    reciprocal => perspective,
-    set_port   => pool_set(0));
+    reciprocal => reciprocal_sg,
+    set_port   => reciprocal_set);
 
-inv_z <= -signed(z);
 --------------------------------------------------------------------------
 -- Multiply Perspective Matrix against points  
 --------------------------------------------------------------------------
-Xc: multiplier_24x24
- port map(
-    clk_port   => clk_port,
-    load_port  => pool_en,
-    reset_port => reset_en,
-    A          => x,
-    B          => m00,
-    A_dig      => d12,
-    B_dig      => d12,
-    AB         => points(0),
-    AB_dig     => OPEN,
-    set_port   => pool_set(1)); 
-
-Yc: multiplier_24x24
- port map(
-    clk_port   => clk_port,
-    load_port  => pool_en,
-    reset_port => reset_en,
-    A          => y,
-    B          => m11,
-    A_dig      => d12,
-    B_dig      => d12,
-    AB         => points(1),
-    AB_dig     => OPEN,
-    set_port   => pool_set(2)); 
-
-pool_done <= '1' when pool_set = "111" else '0';
-
---------------------------------------------------------------------------
--- Perspective Divide  
---------------------------------------------------------------------------
-divide_Xc: multiplier_24x24
- port map(
-    clk_port   => clk_port,
-    load_port  => divide_en,
-    reset_port => reset_en,
-    A          => points(0),
-    B          => perspective,
-    A_dig      => d12,
-    B_dig      => d12,
-    AB         => divided_points(0),
-    AB_dig     => OPEN,
-    set_port   => divide_set(0)); 
-
-divide_Yc: multiplier_24x24
- port map(
-    clk_port   => clk_port,
-    load_port  => divide_en,
-    reset_port => reset_en,
-    A          => points(1),
-    B          => perspective,
-    A_dig      => d12,
-    B_dig      => d12,
-    AB         => divided_points(1),
-    AB_dig     => OPEN,
-    set_port   => divide_set(1)); 
-
-divide_done <= '1' when divide_set = "11" else '0';
-
---------------------------------------------------------------------------
--- Intercept & Round Logic 
---------------------------------------------------------------------------
-affine_points: process( divided_points, intercept_en ) 
-  variable round    : signed(23 downto 0) := x"000800";
-  variable x_round  : signed(23 downto 0) := (others => '0');
-  variable y_round  : signed(23 downto 0) := (others => '0');
-
-  variable x_packet : unsigned(7 downto 0) := (others => '0');
-  variable y_packet : unsigned(7 downto 0) := (others => '0');
+Xc <= m00 * signed(x); 
+Yc <= m11 * signed(y); 
+Wc_reciprocal <= signed(reciprocal_sg);
+process( clk_port )
+  variable round  : signed(23 downto 0) := x"000800"; 
+  variable tx, ty : signed(23 downto 0) := (others => '0');
 begin 
-  round    := x"000800";
-  x_packet := (others => '0');
-  y_packet := (others => '0');
-  x_round  := (others => '0');
-  y_round  := (others => '0');
+  round := x"000800";
+  tx    := (others => '0'); 
+  ty    := (others => '0');
 
-  if intercept_en = '1' then 
-    x_round := signed(divided_points(0));
-    if x_round(23) = '1' then 
-      round := -round; 
-    end if; 
-   
-    x_round := shift_right( shift_left(x_round, 7) + round, 12) + b;
+  if rising_edge(clk_port) then 
+    if reset_port = '1' then 
+      xndc <= (others => '0');
+      yndc <= (others => '0');
+      divide_set <= '0';
+    elsif reciprocal_set = '1' then 
+      xndc <= Xc * Wc_reciprocal;
+      yndc <= Yc * Wc_reciprocal;
+      divide_set <= '1'; 
+    elsif divide_set = '1' then 
+      tx := xndc; 
+      if tx(23) = '1' then 
+        round := -round; 
+      end if; 
+      tx := shift_right( shift_left(tx, 7) + round, 12) + b;
 
-    round := x"000800";
-    y_round := -signed(divided_points(1));
-    if y_round(23) = '1' then 
-      round := -round; 
-    end if; 
-   
-    y_round := shift_right( shift_left(y_round, 7) + round, 12) + b;
-    
-    transl_points(15 downto 8) <= std_logic_vector(x_round(7 downto 0));
-    transl_points(7 downto 0)  <= std_logic_vector(y_round(7 downto 0));
-  end if; 
-end process affine_points; 
+      round := x"000800";
+      ty := yndc; 
+      if ty(23) = '1' then 
+        round := -round; 
+      end if; 
+      ty := shift_right( shift_left(ty, 7) + round, 12) + b;
 
-save_point: process( clk_port ) 
-begin 
-  if rising_edge( clk_port ) then 
-    if reset_en = '1' then 
-      intercept_done <= '0'; 
-      point_packet <= (others => '0');
-    elsif intercept_en = '1' or set_en = '1' then  
-      intercept_done <= '1';  
-      point_packet <= transl_points;
+      -- latch point packet 
+      point_packet <= std_logic_vector(ty(7 downto 0)) 
+                      & std_logic_vector(tx(7 downto 0));
     end if; 
   end if; 
-end process save_point; 
+end process;
 
---------------------------------------------------------------------------
--- FSM Logic 
---------------------------------------------------------------------------
-next_state_logic: process( current_state, reset_port, load_port, pool_done, divide_done, intercept_done )
-begin 
-  if reset_port = '1' then 
-    next_state <= idle; 
-  else 
-    case ( current_state ) is 
-      when idle => 
-        next_state <= idle; 
-        if load_port = '1' then 
-          next_state <= pool_vals; 
-        end if;
-      when pool_vals =>
-        next_state <= pool_vals; 
-        if pool_done = '1' then 
-          next_state <= divide; 
-        end if; 
-      when divide => 
-        next_state <= divide; 
-        if divide_done = '1' then 
-          next_state <= intercept; 
-        end if; 
-      when intercept => 
-        next_state <= intercept; 
-        if intercept_done = '1' then 
-          next_state <= done; 
-        end if; 
-      when done => 
-        next_state <= idle; 
-    end case; 
-  end if; 
-end process next_state_logic; 
-
-set_port <= set_en; 
-
-output_logic: process( current_state )
-begin 
-  divide_en    <= '0'; 
-  pool_en      <= '0';
-  intercept_en <= '0'; 
-  set_en       <= '0';
-  reset_en     <= '0';
-
-  case ( current_state ) is 
-    when idle => 
-      reset_en <= '1';
-    when pool_vals =>
-      pool_en <= '1';
-    when divide => 
-      divide_en <= '1'; 
-    when intercept => 
-      intercept_en <= '1'; 
-    when done => 
-      set_en <= '1';
-  end case; 
-
-end process output_logic; 
-
-update_state: process( clk_port )
-begin 
-  if rising_edge( clk_port ) then 
-    current_state <= next_state; 
-  end if; 
-end process update_state; 
+set_port <= '1' when divide_set = '1' else '0';
 
 end architecture behavioral;
