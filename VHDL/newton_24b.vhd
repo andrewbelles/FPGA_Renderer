97910c1f7e964caf73d9d16de4ac53ddfa68f96d
andrewbelles <andrewbelles31@gmail.com>
2025-08-24 23:22:49 -0400

    piped difference

--- VHDL/newton_24b.vhd
+++ VHDL/newton_24b.vhd
@@ -16,10 +16,11 @@ end entity newton_24b;
 
 architecture behavioral of newton_24b is 
 ----------------------- declarations -------------------------------------
-  type state_type is ( idle, mulA, mulB, done ); 
+  type state_type is ( idle, mulA, diff, mulB, done ); 
   signal current_state, next_state : state_type := idle;
 
   signal mulA_en    : std_logic := '0';
+  signal diff_en    : std_logic := '0'; 
   signal mulB_en    : std_logic := '0'; 
   signal set_en     : std_logic := '0'; 
   signal reset_en   : std_logic := '0'; 
@@ -27,7 +28,9 @@ architecture behavioral of newton_24b is
   signal x0         : signed(23 downto 0) := (others => '0');
   signal x1, x2     : signed(23 downto 0) := (others => '0');
   signal x1_partial : signed(23 downto 0) := (others => '0');
+  signal x1_diff    : signed(23 downto 0) := (others => '0');
   signal x2_partial : signed(23 downto 0) := (others => '0');
+  signal x2_diff    : signed(23 downto 0) := (others => '0');
   signal m          : signed(23 downto 0) := (others => '0');
 
 -- 2 represented as a 11.12 fixed point value 
@@ -40,35 +43,38 @@ m  <= signed(mantissa);
 
 newtons_method: process( clk_port ) 
   variable x48bus : signed(47 downto 0) := (others => '0'); 
-  variable x24bus : signed(23 downto 0) := (others => '0'); 
 begin 
   x48bus := (others => '0'); 
   if rising_edge( clk_port ) then  
     if reset_en = '1' then 
       x1 <= (others => '0'); 
       x1_partial <= (others => '0');
+      x1_diff <= (others => '0');
       x2 <= (others => '0');
       x2_partial <= (others => '0');
+      x2_diff <= (others => '0');
     elsif counter = "10" then 
       counter <= "00"; -- go back to low one cycle after  
     elsif load_port = '1' then 
       if counter = "00" then 
         if mulA_en = '1' then 
           x48bus := m * x0; -- 1.22 * 6.17 -> 11.12 requires 22+17=39, 27 shifts
-          x24bus := shift_right(x48bus, 22)(23 downto 0); 
-          x1_partial <= two_1112 - x24bus; 
+          x1_partial <= shift_right(x48bus, 22)(23 downto 0); 
         elsif mulB_en = '1' then 
-          x48bus := x0 * x24bus; 
+          x1_diff <= two_1112 - x1_partial; 
+        elsif mulB_en = '1' then 
+          x48bus := x0 * x1_diff; 
           x1 <= shift_right(x48bus, 17)(23 downto 0); -- 17 + 12 -> 12 
           counter <= counter + 1; 
         end if; 
       elsif counter = "01" then 
         if mulA_en = '1' then 
           x48bus := m * x1; -- 1.22 * 6.17 -> 11.12 requires 22+17=39, 27 shifts
-          x24bus := shift_right(x48bus, 22)(23 downto 0); 
-          x2_partial <= two_1112 - x24bus; 
+          x2_partial <= shift_right(x48bus, 22)(23 downto 0); 
+        elsif diff_en = '1' then 
+          x2_diff <= two_1112 - x2_partial; 
         elsif mulB_en = '1' then 
-          x48bus := x1 * x24bus; 
+          x48bus := x0 * x2_diff; 
           x2 <= shift_right(x48bus, 17)(23 downto 0); -- 17 + 12 -> 12 
           counter <= counter + 1; 
         end if; 
@@ -92,6 +98,8 @@ begin
       when idle =>
         next_state <= mulA;
       when mulA => 
+        next_state <= diff;
+      when diff => 
         next_state <= mulB; 
       when mulB => 
         if counter = "10" then 
@@ -111,6 +119,7 @@ output_logic: process( current_state )
 begin 
   reset_en <= '0';
   mulA_en  <= '0'; 
+  diff_en  <= '0'; 
   mulB_en  <= '0'; 
   set_en   <= '0'; 
   case ( current_state ) is 
@@ -118,6 +127,8 @@ begin
       reset_en <= '1';  
     when mulA => 
       mulA_en <= '1'; 
+    when diff => 
+      diff_en <= '1'; 
     when mulB => 
       mulB_en <= '1'; 
     when done => 
